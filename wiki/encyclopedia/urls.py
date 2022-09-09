776e46aaae63f2a62f31f0c2797a0da745beca06
Zachery Miller <zacherymiller25@gmail.com>
2022-09-09 14:15:16 -0400

    added search feature

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+# place display markdown url on bottom of url patterns list otherwise it interferes with other links given its design
 urlpatterns = [
     path("", views.index, name="index"),
     path("newpage/", views.newpage, name="newpage"),
     path("randompage/", views.randompage, name="randompage"),
+    path("searchresults/", views.search, name="search"),
+    path("edit/", views.edit, name="edit"),
     path("<str:title>/", views.display_markdown, name="display_markdown")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import render, HttpResponse, redirect
 from random import randint
 
 from . import util
@@ -15,6 +16,7 @@ def newpage(request):
     return render(request, "encyclopedia/newpage.html")
 
 def randompage(request):
+    # pull random entry from entries and convert markdown to HTML
     entries = util.list_entries()
     title = entries[randint(0, len(entries) - 1)]
     entry = markdown2.markdown(util.get_entry(title))
@@ -23,7 +25,58 @@ def randompage(request):
     })
 
 def display_markdown(request, title):
-    entry = markdown2.markdown(util.get_entry(title))
-    return render(request, "encyclopedia/entry.html", {
-        "content": entry, "title": title
-    })
+    # see if entry exists
+    try:
+        entry = markdown2.markdown(util.get_entry(title))
+        return render(request, "encyclopedia/entry.html", {
+            "content": entry, "title": title
+        })
+    
+    # redirect to index if page does not exist when accessed via hardcoded URL
+    except TypeError:
+        return redirect('index')
+
+def search(request):
+    if request.method == "POST":
+        # get query from POST
+        title = request.POST.get('q')
+
+        # get encylcopedia entries
+        entries = util.list_entries()
+
+        # initialize search results list
+        search_results = []
+        
+        # check if exact entry exists by search and render page if so
+        for entry in entries:
+            if title.lower() == entry.lower():
+                searched_entry = markdown2.markdown(util.get_entry(title))
+                return render(request, "encyclopedia/entry.html", {
+                    "content": searched_entry, "title": title
+                })
+            
+            # checks if search is a substring of existing entry. if so, entry is add to search_results list
+            elif entry.lower().find(title.lower()) != -1:
+                search_results.append(entry)
+
+            # continue looping if above conditions are not met
+            else:
+                continue
+        
+        # if entry is not found, return all results where search matches a substring of an existing entry
+        return render(request, "encyclopedia/results.html", {
+            "results": search_results
+        })
+
+    # redirect to index if accessed via GET / absolute URL
+    else:
+        return redirect('index')
+
+
+def edit(request):
+    if request.method == "POST":
+        return HttpResponse("Edit GET!")
+
+    # redirect to index if accessed via GET / absolute URL
+    else:
+        return redirect('index')
