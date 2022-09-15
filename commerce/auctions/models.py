5de9a5f4cefd5bf7d0df1e7904c5ef292a4a086c
Zachery Miller <zacherymiller25@gmail.com>
2022-09-14 20:55:40 -0400

    added models - still need to update

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+# listing model
+class Listing(models.Model):
+    # a listing object will have a title, description, starting bid, a user (who creates the listing), and optionally a category and/or image URL
+    pass
+
+# bid model
+class Bid(models.Model):
+    # a bid object will have a listing, an amount, and a user
+    pass
+
+# comment model
+class Comment(models.Model):
+    # a comment object will have a listing, content, and a user
+    pass
+
+# watching model
+class Watching(models.Model):
+    # a watching object will have a listing and a user
+    pass
