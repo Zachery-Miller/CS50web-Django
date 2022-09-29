97e223ad9a8cb3e03714b3bb523dbc4592393b7d
Zachery Miller <zacherymiller25@gmail.com>
2022-09-29 15:04:00 -0400

    send email function and css for email view

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,8 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Send email
+  document.querySelector('#compose-form').onsubmit = send_email;
+
   // By default, load the inbox
   load_mailbox('inbox');
+
 });
 
 function compose_email() {
@@ -30,4 +34,50 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
+
+  // get request users inbox and display emails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails)
+
+    // loop through and display emails in a new div element each time 
+    for (let i = 0; i < emails.length; i++) {
+      // create new div element and add subject line
+      const email = document.createElement('div');
+      email.classList.add('email-preview');
+      email.innerHTML = emails[i].subject;
+      //email.innerHTML += emails[i].sender;
+      //email.innerHTML += emails[i].timestamp;
+      email.addEventListener('click', function () {
+        console.log('This element has been clicked!')
+      })
+      document.querySelector('#emails-view').append(email)
+    }
+  })
+  }
+
+  function send_email() {
+    // get values from form
+    const recipients = document.querySelector('#compose-recipients').value
+    const subject = document.querySelector('#compose-subject').value
+    const body = document.querySelector('#compose-body').value
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: recipients,
+          subject: subject,
+          body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+
+        // Load sent mailbox
+        load_mailbox('sent');
+    });
+    return false;
+  }
