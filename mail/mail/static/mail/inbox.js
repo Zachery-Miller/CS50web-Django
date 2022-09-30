2bc235c9458932250d8476eedbcfaa94d7c85df4
Zachery Miller <zacherymiller25@gmail.com>
2022-09-29 20:54:26 -0400

    created read_email function and formatted email being read

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -18,6 +18,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#read-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -31,6 +32,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#read-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -48,52 +50,35 @@ function load_mailbox(mailbox) {
       // create child element for sender
       const sender = document.createElement('strong');
       sender.innerHTML = email["sender"];
+      sender.classList.add('email-preview-sender');
       preview.appendChild(sender);
       
       // create child element for subject
       const subject = document.createElement('strong');
       subject.innerHTML = email["subject"];
+      subject.classList.add('email-preview-subject');
       preview.appendChild(subject);
 
       // create child element for timestamp
       const timestamp = document.createElement('strong');
       timestamp.innerHTML = email["timestamp"];
+      timestamp.classList.add('email-preview-timestamp');
       preview.appendChild(timestamp);
 
       // add event listener
-      preview.addEventListener('click', function () {
-        console.log('This element has been clicked!');
-      })
+      preview.addEventListener('click', function(){ read_email(email); });
+
       // add email to emails view
       document.querySelector('#emails-view').append(preview);
     })
   })
-}
-
-
-    /*
-    // loop through and display emails in a new div element each time 
-    for (let i = 0; i < emails.length; i++) {
-      // create new div element and add subject line
-      const email = document.createElement('div');
-      email.classList.add('email-preview');
-      email.innerHTML = emails[i].subject;
-      //email.innerHTML += emails[i].sender;
-      //email.innerHTML += emails[i].timestamp;
-      email.addEventListener('click', function () {
-        console.log('This element has been clicked!')
-      })
-      document.querySelector('#emails-view').append(email)
-    }
-  })
   }
-*/
 
   function send_email() {
     // get values from form
-    const recipients = document.querySelector('#compose-recipients').value
-    const subject = document.querySelector('#compose-subject').value
-    const body = document.querySelector('#compose-body').value
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
 
     fetch('/emails', {
       method: 'POST',
@@ -112,4 +97,49 @@ function load_mailbox(mailbox) {
         load_mailbox('sent');
     });
     return false;
+  }
+
+  function read_email(email) {
+    // Show read view and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#read-view').style.display = 'block';
+
+    // default open mail name
+    document.querySelector('#read-view').innerHTML = `<h3>Now reading...</h3>`;
+
+    // create display for email info
+    const sender = document.createElement('small');
+    sender.innerHTML = `from: ${email.sender}`
+
+    const recipients = document.createElement('small');
+    recipients.innerHTML = `to: ${email.recipients}`
+
+    const subject = document.createElement('h4');
+    subject.innerHTML = `${email.subject}`
+
+    const timestamp = document.createElement('small');
+    timestamp.innerHTML = `${email.timestamp}`
+    timestamp.id = "email-timestamp";
+
+    const body = document.createElement('div');
+    body.innerHTML = `${email.body}`
+
+    const pagebreak = document.createElement('hr');
+
+    const subjectdiv = document.createElement('div');
+    subjectdiv.id = "email-subject";
+    subjectdiv.appendChild(subject);
+
+    const upperdiv = document.createElement('div');
+    upperdiv.appendChild(sender);
+    upperdiv.appendChild(timestamp);
+
+    const lowerdiv = document.createElement('div');
+    lowerdiv.appendChild(recipients);
+
+    // add display info to read-view
+    document.querySelector('#read-view').append(subjectdiv, upperdiv, lowerdiv, pagebreak, body);
+
+    console.log(`The id of this email is ${email.id}`);
   }
