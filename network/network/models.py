cce22bc56bc11670694a3af9ef04b41af4acd743
Zachery Miller <zacherymiller25@gmail.com>
2022-10-01 15:43:25 -0400

    fixed db migrations

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    #auto_now will update timestamp every time model is saved, so when user edits post it will refresh the post to the top of the feed chronologically
+    timestamp = models.DateTimeField(auto_now=True)
+
+class Likes(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    user_followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
